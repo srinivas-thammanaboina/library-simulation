7cd26968b66b5a83425ab02378e38a01977ca1c0
Srinivas <Srinivasthammana99@gmail.com>
2017-12-02 01:12:16 -0700

    adding constants class

---
+++ src/librarysimulation/Constants.java
@@ -0,0 +1,11 @@
+package librarysimulation;
+
+public class Constants {
+
+	public static final int NUMBER_OF_STUDENTS = 5;
+	
+	public static final int NUMBER_OF_Books = 5;
+	
+	//public static final int NUMBER_OF_STUDENTS = 5;
+	
+}
